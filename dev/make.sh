05ec791630d702fed26808cbd6c3f41951dab082
Andy Shu <shu@immux.com>
2020-04-11 14:22:43 +0800

    Add daily backup

--- dev/make.sh
+++ dev/make.sh
@@ -10,5 +10,6 @@ ${webpack} --config src/client/web/webpack.config.prod.js
 
 cp ops/pm2.prod.config.js ${BUILD_ROOT}/
 cp ops/update_app.sh ${BUILD_ROOT}/
+cp ops/backup.sh ${BUILD_ROOT}/
 cp package.json ${BUILD_ROOT}/
 cp package-lock.json ${BUILD_ROOT}/

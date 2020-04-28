35c1cf3e348b626b60b62f3147e5b2220f731817
Andy Shu <shu@immux.com>
2020-04-28 16:06:16 +0800

    Change

--- ops/deploy_nginx.sh
+++ ops/deploy_nginx.sh
@@ -1,5 +1,5 @@
-SERVER="maltaa.org"
+SERVER="yokota"
 SERVER_AUTH="deploy@${SERVER}"
 
-scp -P 233 ops/*.nginx ${SERVER_AUTH}:/tmp/;
-ssh -p 233 -t ${SERVER_AUTH} "sudo cp /tmp/*.nginx /etc/nginx/ && sudo nginx -t && sudo nginx -s reload";
+scp -P 222 ops/*.nginx ${SERVER_AUTH}:/tmp/;
+ssh -p 222 -t ${SERVER_AUTH} "sudo cp /tmp/*.nginx /etc/nginx/ && sudo nginx -t && sudo nginx -s reload";

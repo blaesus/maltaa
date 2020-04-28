9a5b8c6fa866c7cea0fe9ff9554a59bee7ad7554
Andy Shu <shu@immux.com>
2020-04-28 15:47:05 +0800

    Refine

--- ops/push.sh
+++ ops/push.sh
@@ -1,2 +1,2 @@
-rsync -avz  -e "ssh -p 233" built/dist/ deploy@narita:/var/maltaa
-ssh -p 233 deploy@narita "cd /var/maltaa && sh update_app.sh"
+rsync -avz  -e "ssh -p 233" built/dist/ deploy@yokota:/var/maltaa
+ssh -p 222 deploy@yokota "cd /var/maltaa && sh update_app.sh"

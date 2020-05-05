9e015f570f53722a2d79365d8eb2d1486d249d3d
Andy Shu <shu@immux.com>
2020-05-05 11:36:22 +0800

    Fix port

--- ops/push.sh
+++ ops/push.sh
@@ -1,2 +1,2 @@
-rsync -avz  -e "ssh -p 233" built/dist/ deploy@yokota:/var/maltaa
+rsync -avz  -e "ssh -p 222" built/dist/ deploy@yokota:/var/maltaa
 ssh -p 222 deploy@yokota "cd /var/maltaa && sh update_app.sh"
